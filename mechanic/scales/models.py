8e10289c54b0a3e604e58541c343fd0afffda13e
oliverosborne9 <oliver@recycleye.com>
2022-09-14 23:00:29 +0100

    fix: restructure scales module to fix circular imports and improve structure

--- mechanic/scales/models.py
+++ mechanic/scales/models.py
@@ -1,36 +1,9 @@
 import time
-from abc import ABC, abstractmethod
 from datetime import datetime
 from random import randrange
 
-
-class Scale(ABC):
-    def __init__(self, path: str):
-        self.path = path
-        self.name = path.split("/")[-1]
-
-    @abstractmethod
-    def get_grams(self):
-        pass
-
-    @abstractmethod
-    def get_weight_tuple(self):
-        pass
-
-    def get_status(self):
-        return "ok" if self.get_grams() is not None else "unavailable"
-
-    def get_mass_response(self):
-        return {
-            "mass": self.get_grams(),
-            "time": str(datetime.utcnow()),
-            "name": self.name,
-            "status": self.get_status(),
-        }
-
-    @classmethod
-    def from_name(cls, name: str) -> "Scale":
-        return cls(f"/dev/{name}")
+from mechanic.scales.base import Scale
+from mechanic.scales.hid import DymoScale
 
 
 class FakeScale(Scale):
@@ -53,4 +26,4 @@ class FakeScale(Scale):
 # has been authored by @drew65 but is omitted from this project
 # because the code is intellectual property owned by Recycleye.
 
-SCALES_MODELS = {"FAKE": FakeScale}
+SCALES_MODELS = {"FAKE": FakeScale, "DYMO": DymoScale}
